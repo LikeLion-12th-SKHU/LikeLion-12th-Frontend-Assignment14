7b19b330b518ab25c3e16a5983aedd6b683d92d3
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-10-25 21:45:16 +0900

    done

---
+++ yeeun/weather-simulation/src/components/WeatherSelect.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const WeatherSelect = ({ onWeatherChange }) => {
+  return (
+    <div className='weather-select'>
+      <label htmlFor='weather'>날씨 선택:</label>
+      <select
+        id='weather'
+        onChange={(e) => onWeatherChange(e.target.value)}>
+        <option value='sunny'>맑음</option>
+        <option value='rainy'>비</option>
+        <option value='snowy'>눈</option>
+        <option value='cloudy'>구름</option>
+        <option value='tornado'>토네이도</option>
+      </select>
+    </div>
+  );
+};
+
+export default WeatherSelect;

54c05e0a8d17910e5737d295c61b0485ee0bec75
Yun, HyunSeung <hyun136000@gamil.com>
2024-10-29 22:54:30 +0900

    14

---
+++ weather/src/components/WeatherSelect.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const WeatherSelect = ({ onWeatherChange }) => {
+  return (
+    <div className="weather-select">
+      <label htmlFor="weather">날씨 선택:</label>
+      <select id="weather" onChange={(e) => onWeatherChange(e.target.value)}>
+        <option value="sunny">맑음</option>
+        <option value="rainy">비</option>
+        <option value="snowy">눈</option>
+        <option value="cloudy">구름</option>
+        <option value="thunderstorm">번개</option>
+      </select>
+    </div>
+  );
+};
+
+export default WeatherSelect;
